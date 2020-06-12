270b37d95729445ecbe419b3ea30ab4f1b59d5b2
Thalia Nero <chrisvn00@gmail.com>
2020-06-11 20:18:02 -0400

    Document Lifecycle

--- src/main/java/com/mojang/serialization/Lifecycle.java
+++ src/main/java/com/mojang/serialization/Lifecycle.java
@@ -2,6 +2,13 @@
 // Licensed under the MIT license.
 package com.mojang.serialization;
 
+/**
+ * A marker defining the stability of a given result. {@linkplain #experimental() Experimental} results are may changed
+ * incompatibly in the future and {@linkplain #deprecated(int) deprecated} results may be removed in the future.
+ *
+ * @see Codec#withLifecycle(Lifecycle)
+ * @see DataResult#setLifecycle(Lifecycle)
+ */
 public class Lifecycle {
     private static final Lifecycle STABLE = new Lifecycle() {
         @Override
@@ -19,30 +26,70 @@ public class Lifecycle {
     private Lifecycle() {
     }
 
+    /**
+     * A class for {@linkplain Lifecycle#deprecated(int) deprecated} lifecycles.
+     *
+     * @see Lifecycle#deprecated(int)
+     */
     public static final class Deprecated extends Lifecycle {
         private final int since;
 
+        /**
+         * Constructs a deprecated lifecycle with the given starting version.
+         *
+         * @param since The version that the result was deprecated in.
+         */
         public Deprecated(final int since) {
             this.since = since;
         }
 
+        /**
+         * Returns the version since the result was deprecated.
+         */
         public int since() {
             return since;
         }
     }
 
+    /**
+     * Returns the experimental lifecycle.
+     */
     public static Lifecycle experimental() {
         return EXPERIMENTAL;
     }
 
+    /**
+     * Returns the stable lifecycle.
+     */
     public static Lifecycle stable() {
         return STABLE;
     }
 
+    /**
+     * Returns a deprecated lifecycle for the given version.
+     *
+     * @param since The version the lifecycle is for.
+     * @see Deprecated
+     */
     public static Lifecycle deprecated(final int since) {
         return new Deprecated(since);
     }
 
+    /**
+     * Combines this lifecycle with another lifecycle. The lifecycle with the highest potential for breakage
+     * (either {@code this} or {@code other}) is returned.
+     *
+     * <p>For example, the following lifecycles are ordered according to breakage potential.
+     * <ol>
+     *     <li>{@code Experimental}</li>
+     *     <li>{@code Deprecated(since = 1)}</li>
+     *     <li>{@code Deprecated(since = 2)}</li>
+     *     <li>{@code Stable}</li>
+     * </ol>
+     *
+     * @param other The lifecycle to compare this lifecycle against.
+     * @return The lifecycle with the highest "breakage potential".
+     */
     public Lifecycle add(final Lifecycle other) {
         if (this == EXPERIMENTAL || other == EXPERIMENTAL) {
             return EXPERIMENTAL;
