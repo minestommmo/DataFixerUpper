0cc2cafda54faae7f547f2c45bf1ef9f97c0a060
Thalia Nero <chrisvn00@gmail.com>
2020-06-20 01:30:57 -0400

    Document Wander

--- src/main/java/com/mojang/datafixers/optics/Wander.java
+++ src/main/java/com/mojang/datafixers/optics/Wander.java
@@ -7,6 +7,28 @@ import com.mojang.datafixers.kinds.App;
 import com.mojang.datafixers.kinds.Applicative;
 import com.mojang.datafixers.kinds.K1;
 
+/**
+ * A generalization of {@link com.mojang.datafixers.kinds.Traversable} for any object types, not just functors.
+ * This interface is used in {@link Traversal} to implement optics that visit each field of a traversable structure.
+ *
+ * @param <S> The input object type.
+ * @param <T> The output object type.
+ * @param <A> The input field type.
+ * @param <B> The output field type.
+ * @dfu.shape %.Type.[%0::%2,%1::%3]
+ * @see com.mojang.datafixers.kinds.Traversable
+ */
 public interface Wander<S, T, A, B> {
+    /**
+     * Takes the given {@link Applicative}-effectful function over the field types and produces an effectful function
+     * over the object types. The returned function traverses the structure {@code S}, applies the transformation to
+     * each field in {@code S}, and combines the resulting effects into a single effect containing the transformed
+     * object {@code T}.
+     *
+     * @param applicative The {@link Applicative} type class instance defining the behavior of {@code F}.
+     * @param input       A function from the input field type to the output field type.
+     * @param <F>         The functor produced by the given function.
+     * @return A function from the input object type to the output object type.
+     */
     <F extends K1> FunctionType<S, App<F, T>> wander(final Applicative<F, ?> applicative, final FunctionType<A, App<F, B>> input);
 }
