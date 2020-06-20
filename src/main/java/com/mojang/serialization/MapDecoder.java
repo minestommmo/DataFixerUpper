554d91dcc03d3a8ae6839cefccda687fdf4f7e0e
Thalia Nero <chrisvn00@gmail.com>
2020-06-20 18:45:03 -0400

    Document MapDecoder

--- src/main/java/com/mojang/serialization/MapDecoder.java
+++ src/main/java/com/mojang/serialization/MapDecoder.java
@@ -13,9 +13,48 @@ import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * Deserializes (decodes) a fixed set of record fields from a serialized form.
+ *
+ * <p>A {@link MapDecoder} is a specialization of a {@link Decoder} that operates on some defined set of record keys.
+ * While a {@link MapDecoder} does not implement {@link Decoder}, any {@link MapDecoder} may be turned into a
+ * {@link Decoder} by calling the {@link #decoder()} method.
+ *
+ * <p>Implementations of {@link MapDecoder} are immutable once created. The methods defined in this interface never
+ * mutate the decoder in a way visible to the outside.
+ *
+ * @param <A> The type that this {@link MapDecoder} deserializes.
+ * @implNote The default methods in this interface that return map decoders or {@linkplain Decoder decoders}
+ * wrap this decoder without adding any debugging context. These methods should be overridden if deeply nested
+ * decoders are undesirable or additional debugging context is desired.
+ * @see Decoder
+ * @see MapCodec
+ */
 public interface MapDecoder<A> extends Keyable {
+    /**
+     * Decodes an object from the given map of serialized entries. If decoding fails (for example, if a key is absent
+     * or has an incompatible value), returns an error {@link DataResult}.
+     *
+     * @param ops   The {@link DynamicOps} instance defining the serialized form.
+     * @param input A map or maplike object containing the serialized entries.
+     * @param <T>   The type of the serialized form.
+     * @return A {@link DataResult} containing the decoded object, or an error if no object could be decoded.
+     * @see #compressedDecode(DynamicOps, Object)
+     */
     <T> DataResult<A> decode(DynamicOps<T> ops, MapLike<T> input);
 
+    /**
+     * Decodes an object from the given serialized form. If the {@link DynamicOps} uses compressed keys, the input
+     * should also contain compressed keys. Likewise, if the {@link DynamicOps} does not use compressed keys, the input
+     * should also not contain compressed keys.
+     *
+     * @param ops   The {@link DynamicOps} instance defining the serialized form.
+     * @param input The serialized value that contains the record data to deserialize.
+     * @param <T>   The type of the serialized form.
+     * @return A {@link DataResult} containing the decoded object, or an error if no object could be decoded.
+     * @implSpec The default implementation determines whether the serialized keys are compressed. Then, it deserializes
+     * the input into a {@link MapLike} that is then passed to {@link #decode(DynamicOps, MapLike)}.
+     */
     default <T> DataResult<A> compressedDecode(final DynamicOps<T> ops, final T input) {
         if (ops.compressMaps()) {
             final Optional<Consumer<Consumer<T>>> inputList = ops.getList(input).result();
@@ -52,8 +91,21 @@ public interface MapDecoder<A> extends Keyable {
         return ops.getMap(input).setLifecycle(Lifecycle.stable()).flatMap(map -> decode(ops, map));
     }
 
+    /**
+     * Returns the {@link KeyCompressor} used to associate record keys with indices.
+     *
+     * @param ops The {@link DynamicOps} instance defining the serialized form.
+     * @param <T> The type of the serialized form.
+     * @return The {@link KeyCompressor} for this map decoder.
+     */
     <T> KeyCompressor<T> compressor(DynamicOps<T> ops);
 
+    /**
+     * Returns a {@link Decoder} that implements the operations defined in this map decoder.
+     *
+     * @implNote The default implementation returns a {@link Decoder} that implements the {@link Decoder#decode(DynamicOps, Object)}
+     * method to call {@link #decode(DynamicOps, MapLike)}.
+     */
     default Decoder<A> decoder() {
         return new Decoder<A>() {
             @Override
@@ -68,6 +120,15 @@ public interface MapDecoder<A> extends Keyable {
         };
     }
 
+    /**
+     * Transforms this map decoder to operate on a different type using the given partial mapping function. Any errors
+     * the mapping function returns are merged with errors from deserialization.
+     *
+     * @param function A function from the current type to the new type.
+     * @param <B>      The new type.
+     * @return A map decoder that decodes using this map decoder, then transforms the output using the mapping function.
+     * @implSpec The default implementation returns a {@link MapDecoder.Implementation} that wraps this map decoder.
+     */
     default <B> MapDecoder<B> flatMap(final Function<? super A, ? extends DataResult<? extends B>> function) {
         return new Implementation<B>() {
             @Override
@@ -87,6 +148,14 @@ public interface MapDecoder<A> extends Keyable {
         };
     }
 
+    /**
+     * Transforms this map decoder to operate on a different type using the given mapping function.
+     *
+     * @param function A function from the current type to the new type.
+     * @param <B>      The new type.
+     * @return A map decoder that decodes using this map decoder, then transforms the output using the mapping function.
+     * @implSpec The default implementation returns a {@link MapDecoder.Implementation} that wraps this map decoder.
+     */
     default <B> MapDecoder<B> map(final Function<? super A, ? extends B> function) {
         return new Implementation<B>() {
             @Override
@@ -106,6 +175,19 @@ public interface MapDecoder<A> extends Keyable {
         };
     }
 
+    /**
+     * Applies a transformation decoded from the given map decoder to decoded values. Errors from the transformation
+     * decoder are merged with errors from this decoder.
+     *
+     * @param decoder A decoder that deserializes mapping functions.
+     * @param <E>     The new type.
+     * @return A map decoder that deserializes a value, then applies a transformation deserialized from the given
+     * map decoder.
+     * @apiNote This method implements the <em>applicative operator</em> for {@link MapDecoder}.
+     * @implSpec The default implementation returns a {@link MapDecoder.Implementation} that wraps this map decoder.
+     * @see #map(Function)
+     * @see com.mojang.datafixers.kinds.Applicative
+     */
     default <E> MapDecoder<E> ap(final MapDecoder<Function<? super A, ? extends E>> decoder) {
         return new Implementation<E>() {
             @Override
@@ -127,6 +209,14 @@ public interface MapDecoder<A> extends Keyable {
         };
     }
 
+    /**
+     * Sets the {@link Lifecycle} of the results this map decoder deserializes.
+     *
+     * @param lifecycle The lifecycle to use.
+     * @return A map decoder equivalent to this map decoder, but using the given lifecycle for the deserialized
+     * results.
+     * @implSpec The default implementation returns a {@link MapDecoder.Implementation} that wraps this map decoder.
+     */
     default MapDecoder<A> withLifecycle(final Lifecycle lifecycle) {
         return new Implementation<A>() {
             @Override
@@ -146,6 +236,13 @@ public interface MapDecoder<A> extends Keyable {
         };
     }
 
+    /**
+     * A class that implements both {@link MapDecoder} and {@link Compressable}.
+     *
+     * <p>The method {@link MapDecoder#compressor(DynamicOps)} is implemented via {@link CompressorHolder#compressor(DynamicOps)}.
+     *
+     * @param <A> The type that this map decoder deserializes.
+     */
     abstract class Implementation<A> extends CompressorHolder implements MapDecoder<A> {
     }
 }
